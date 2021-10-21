3caff66fb9cac1f459fa2a18393dcbaaf72d2fee
Brian Sohn <kolpp15@gmail.com>
2021-10-21 13:20:05 -0700

    add dummy schema

--- server.js
+++ server.js
@@ -2,15 +2,28 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const {
   GraphQLSchema,
-  GraphQLObjectType
+  GraphQLObjectType,
+  GraphQLString
 } = require('graphql');
 const app = express();
 
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'HelloWorld',
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => 'Hello World'
+      }
+    })
+  })
+});
 
  
 app.use(
   '/graphql',
   graphqlHTTP({
+    schema: schema,
     graphiql: true
   })
 );
