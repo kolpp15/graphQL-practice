15e225100cc22ab952e806df613af26c5a8307e8
Brian Sohn <kolpp15@gmail.com>
2021-10-26 16:48:55 -0700

    added mutation feature for new authors

--- server.js
+++ server.js
@@ -113,9 +113,24 @@ const RootMutationType = new GraphQLObjectType({
         books.push(book);
         return book;
       }
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: 'Add an Author',
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) }
+      },
+      resolve: (parent, args) => {
+        const author = {
+          id: authors.length + 1,
+          name: args.name
+        };
+        authors.push(author);
+        return author;
+      }
     }
   })
-})
+});
 
 const schema = new GraphQLSchema({
   query: RootQueryType,
